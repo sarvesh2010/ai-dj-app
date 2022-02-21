a122ad3c4c5d9077f80c7742bd380699fe3016e2
Sarvuu <76529861+sarvesh2010@users.noreply.github.com>
2022-02-21 18:44:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist = 0;
+
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
@@ -17,6 +19,17 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if (scoreLeftWrist > 0) {
+        
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimal = floor(InNumberleftWristY);
+        volume = remove_decimal / 500;
+        document.getElementById("volume").innerHTML = "volume=" + volume;
+        song.setVolume(volume);
+    }
 }
 
 function preload() {
@@ -36,6 +49,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist=" + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX= " + leftWristX + "leftWristY= " + leftWristY);
